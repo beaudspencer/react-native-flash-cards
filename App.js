08dd96edf0912d38cae8cf749c9f7bc36180df24
Beau Spencer <bdspencer416@gmail.com>
2018-12-14 20:43:40 +0000

    rendered the CreateCard component and defined a handleSubmit method that pushes a card to the App states cards array. #1

--- App.js
+++ App.js
@@ -1,21 +1,38 @@
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React from 'react'
+import { StyleSheet, View } from 'react-native'
+import CreateCard from './src/create-card'
 
 export default class App extends React.Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      cards: []
+    }
+    this.handleSubmit = this.handleSubmit.bind(this)
+  }
+  handleSubmit(card) {
+    const cards = this.state.cards.slice()
+    cards.push(card)
+    this.setState({
+      cards
+    })
+  }
   render() {
     return (
       <View style={styles.container}>
-        <Text>Open up App.js to start working on your app!</Text>
+        <CreateCard
+          handleSubmit={this.handleSubmit}
+        />
       </View>
-    );
+    )
   }
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: '#E0E0E0',
     alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+    justifyContent: 'center'
+  }
+})
