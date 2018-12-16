f459454db74d6b5991b0fc1f585810214008d341
Beau Spencer <bdspencer416@gmail.com>
2018-12-16 00:29:44 +0000

    added a renderView method to the App component that renders views based on the state. imported and displayed the Navi Component. #2

--- App.js
+++ App.js
@@ -1,14 +1,22 @@
 import React from 'react'
 import { StyleSheet, View } from 'react-native'
+import {
+  Content,
+  Container
+} from 'native-base'
 import CreateCard from './src/create-card'
+import CardList from './src/card-list'
+import Navi from './src/navi'
 
 export default class App extends React.Component {
   constructor(props) {
     super(props)
     this.state = {
+      view: 'create',
       cards: []
     }
     this.handleSubmit = this.handleSubmit.bind(this)
+    this.renderView = this.renderView.bind(this)
   }
   handleSubmit(card) {
     const cards = this.state.cards.slice()
@@ -17,12 +25,32 @@ export default class App extends React.Component {
       cards: cards
     })
   }
-  render() {
-    return (
-      <View style={styles.container}>
+  renderView() {
+    const { view, cards } = this.state
+    if (view === 'create') {
+      return (
         <CreateCard
           handleSubmit={this.handleSubmit}
         />
+      )
+    }
+    else if (view === 'list') {
+      return (
+        <CardList
+          cards={cards}
+        />
+      )
+    }
+  }
+  render() {
+    return (
+      <View style={styles.container}>
+        <Container>
+          <Navi/>
+          <Content>
+            {this.renderView()}
+          </Content>
+        </Container>
       </View>
     )
   }
