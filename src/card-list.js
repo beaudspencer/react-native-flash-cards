7451e624c242b516e5ece8782df84a777758f24e
Beau Spencer <bdspencer416@gmail.com>
2018-12-18 01:14:04 +0000

    made container full width and added padder to the content #2

--- src/card-list.js
+++ src/card-list.js
@@ -1,35 +1,41 @@
 import React from 'react'
 import {
-  StyleSheet,
-  View
+  StyleSheet
 } from 'react-native'
+import {
+  Container,
+  Content
+} from 'native-base'
 import FlashCard from './flash-card'
 
 export default class CardList extends React.Component {
   render() {
     const { cards } = this.props
     return (
-      <View
+      <Container
         style={styles.view}
       >
-        {
-          cards.map((card, index) => {
-            return (
-              <FlashCard
-                key={index}
-                card={card}
-              />
-            )
-          })
-        }
-      </View>
+        <Content
+          padder
+        >
+          {
+            cards.map((card, index) => {
+              return (
+                <FlashCard
+                  key={index}
+                  card={card}
+                />
+              )
+            })
+          }
+        </Content>
+      </Container>
     )
   }
 }
 
 const styles = StyleSheet.create({
   view: {
-    flex: 1,
-    flexDirection: 'row'
+    width: '100%'
   }
 })
